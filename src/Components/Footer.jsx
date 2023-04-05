21c134009fb476aad89f290fad0911d00ab8642f
Juan DIego <juand.chaconmarin@gmail.com>
2023-04-05 18:30:27 -0500

    Actualizacion de archivos

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useEstadosGlobalesContext } from "./utils/global.context";
 
 const Footer = () => {
+  const { theme } = useEstadosGlobalesContext();
   return (
-    <footer>
-        <p>Powered by</p>
-        <img src="/images/DH.png" alt='DH-logo' />
+    <footer className={theme.color}>
+      <p>Powered by</p>
+      <img src="./images/DH.png" alt="DH-logo" />
+      <img src="./images/ico-facebook.png" alt="Facebook-logo" class="socialNetwork" />
+      <img src="./images/ico-instagram.png" alt="Instagram-logo" class="socialNetwork" />
+      <img src="./images/ico-tiktok.png" alt="Tiktok-logo" class="socialNetwork" />
+      <img src="./images/ico-whatsapp.png" alt="Whatsapp-logo" class="socialNetwork" />
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
